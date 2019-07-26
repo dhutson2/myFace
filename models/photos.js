4beb8daa8a906c74513e86a68b08b3a9d384aa85
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2019-07-26 09:33:28 -0600

    Photo Schema completed

---
+++ models/photos.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const photoSchema = new Schema({
+	title: String,
+	url: String,
+	description: String,
+	comments: String,
+	user: {
+		type: Schema.Types.ObjectID,
+		ref: 'User'
+	}
+});
