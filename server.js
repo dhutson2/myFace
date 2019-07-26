3b769025ea45aec1274c8fe527171d2bcdf15c5c
Sarah Kim <Sarahckim143@gmail.com>
2019-07-26 10:53:45 -0600

    user model and index

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express')
 const app = express()
 
+
+
+const session =require('express-session');
 require('./db/db')
+
+const usersController = require('./controllers/users');
+
+
+app.use(session({
+	secret: "keepitsecretstring",
+	resave: false,
+	saveUninitialized: false
+}))
+
+
+
+
+app.get('/', (req, res) => {
+	res.render('index.ejs')
+})
+
 
 app.listen(3000, () => {
     console.log('listening on 3k')
