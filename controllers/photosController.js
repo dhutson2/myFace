d77fb0898fd6d50e594f1759a081aa5d7c1ba504
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2019-07-26 14:11:05 -0600

    photos views and routes created

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -6,7 +6,9 @@ const Photo = require('../models/photosModel');
 router.get('/', async (req, res) => {
 	try {
 		const photos = await Photo.find();
-		res.render('photos/index.ejs')		
+		res.render('photos/index.ejs', {
+			photos: photos
+		})		
 	} catch(err){
 		res.send(err)
 	}
@@ -20,54 +22,56 @@ router.get('/new', (req, res) => {
 // Show route
 router.get('/:id', async (req, res) => {
 	try{
-		const photo = await Photo.findById();
-		res.render('photos/show.ejs');
+		const photo = await Photo.findById(req.params.id);
+		res.render('photos/show.ejs', {
+			photo: photo
+		});
 	}catch(err){
 		res.send(err);
 	}
 });
 
 // Post route
-// router.post('/', async (req, res) => {
-// 	try{
-// 		const photo = await Photo.create(req.body);
-// 		res.redirect('/photos');		
-// 	} catch(err){
-// 		res.send(err);
-// 	}
-// });
+router.post('/', async (req, res) => {
+	try{
+		const photo = await Photo.create(req.body);
+		res.redirect('/photos');		
+	} catch(err){
+		res.send(err);
+	}
+});
 
 // Delete route
-// router.delete('/:id', async (req, res) => {
-// 	try{
-// 		await Photo.findByIdAndDelete(req.params.id);
-// 		res.redirect('/photos');
-// 	} catch(err){
-// 		res.send(err);
-// 	}
-// });
+router.delete('/:id', async (req, res) => {
+	try{
+		await Photo.findByIdAndDelete(req.params.id);
+		res.redirect('/photos');
+	} catch(err){
+		res.send(err);
+	}
+});
 
 // Edit route
-// router.get('/:id/edit', async (req, res) => {
-// 	try {
-// 		const photo = await Photo.findById(req.params.id);
-// 		res.render('/photos/edit.ejs', {
-// 			photo: photo
-// 		})
-// 	} catch(err){
-// 		res.send(err)
-// 	}
-// });
+router.get('/:id/edit', async (req, res) => {
+	try {
+		const photo = await Photo.findById(req.params.id);
+		res.render('/photos/edit.ejs', {
+			photo: photo
+		})
+	} catch(err){
+		res.send(err)
+	}
+});
 
 // Update route
-// router.put('/:id', async (req, res) => {
-// 	try {
-// 		const newPhoto = await Photo.findByIdAndUpdate(req.params.id, req.body {new: true});
-// 		res.redirect('/photos/${req.params.id}')		
-// 	} catch(err) {
-// 		res.send(err);
-// 	}
-// });
+router.put('/:id', async (req, res) => {
+	try {
+		const newPhoto = await Photo.findByIdAndUpdate(req.params.id, req.body, {new: true});
+		res.redirect('/photos/${req.params.id}')		
+	} catch(err) {
+		res.send(err);
+	}
+});
 
 
 module.exports = router;
