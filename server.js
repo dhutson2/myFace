6fad8fab0d2ac2df3cbfa7b05f81a021dedb8cf5
Derek Hutson <denverd1301@gmail.com>
2019-07-26 10:41:10 -0600

    added all profile routes w/ descriptions. linked profile controller to server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const session = require('express-session')
 const app = express()
 
 require('./db/db')
+
+const profilesController = require('./controllers/profilesControllers')
+
+
+
+
+app.use('/profile', profilesController)
 
 app.listen(3000, () => {
     console.log('listening on 3k')
